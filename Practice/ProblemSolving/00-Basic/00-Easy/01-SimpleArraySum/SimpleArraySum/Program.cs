4f6a2604f0167194c41d7849ac19fbeb7156f980
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-13 11:04:58 -0300

    Simple Array Sum Exercise

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/01-SimpleArraySum/SimpleArraySum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int result = SimpleArraySum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        static int SimpleArraySum(int[] ar)
+        {
+            var sum = ar.Sum();
+            return sum;
+        }
+    }
+}
