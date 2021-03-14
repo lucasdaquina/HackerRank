d32e222e5fecda44b0f58d7dd5223f25bf688585
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-14 20:40:29 -0300

    Very Big Sum exercise

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/03-VeryBigSum/VeryBigSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace VeryBigSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp));
+            long result = AVeryBigSum(ar);
+        }
+
+
+        static long AVeryBigSum(long[] ar)
+        {
+            return ar.Sum();
+        }
+    }
+}
