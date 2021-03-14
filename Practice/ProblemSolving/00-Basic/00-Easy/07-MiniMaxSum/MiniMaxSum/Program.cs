77305a9ea1f20c206d16b64cd386208363f498e7
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-14 20:56:08 -0300

    Mini Max Sum Exercise

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/07-MiniMaxSum/MiniMaxSum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace MiniMaxSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            MiniMaxSum(arr);
+        }
+
+        static void MiniMaxSum(int[] arr)
+        {
+            var longs = arr.Select(item => (long)item).ToArray();
+            var sortedArray = longs.ToList().OrderBy(_ => _);
+            var max = sortedArray.Skip(1).Take(4).Sum();
+            var min = sortedArray.Take(4).Sum();
+
+            Console.WriteLine(min.ToString() + " " + max.ToString());
+        }
+    }
+}
