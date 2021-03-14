857bf76cd715e0f730f07cc6b8ee2ef6026e0ee0
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-14 20:54:28 -0300

    Stair Case Exercise

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/06-StairCase/StarCase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Staircase(n);
+        }
+
+        static void Staircase(int n)
+        {
+
+            int i = 1;
+            while (i <= n)
+            {
+                var a = new String(' ', n - i);
+                var resh = new String('#', i);
+                Console.WriteLine(a + resh);
+                i++;
+            }
+
+        }
+    }
+}
