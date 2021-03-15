8cb61aea773d93ab98b90b16d929210f9215b505
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-14 21:05:53 -0300

    Time Conversion Exercise

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/09-TimeConversion/TimeConversion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string result = TimeConversion(s);
+        }
+
+        static string TimeConversion(string s)
+        {
+            var isBeforeNoon = s.Contains("AM") ? true : false;
+            s = s.Replace("AM", string.Empty).Replace("PM", string.Empty);
+            var h = s.Split(':');
+            if (isBeforeNoon)
+            {
+                var hora = h[0] == "12" ? "00" : h[0];
+                return $"{hora}:{h[1]}:{h[2]}";
+            }
+            else
+            {
+                var horaTarde = h[0] == "12" ? "12" : (int.Parse(h[0]) + 12).ToString();
+                return $"{horaTarde}:{h[1]}:{h[2]}";
+            }
+
+        }
+    }
+}
