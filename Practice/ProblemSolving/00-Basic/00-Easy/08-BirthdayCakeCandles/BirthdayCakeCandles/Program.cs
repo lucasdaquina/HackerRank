45bd2144b5df407c5c8a76fd5e7ef02702152c4e
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-14 21:01:46 -0300

    Birthday Cake Candles Exercise

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/08-BirthdayCakeCandles/BirthdayCakeCandles/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCakeCandles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+            int result = BirthdayCakeCandles(candles);
+        }
+
+        public static int BirthdayCakeCandles(List<int> candles)
+        {
+            var unit = candles.Max();
+            var qtd = candles.Where(_ => _ == unit).Count();
+            return qtd;
+        }
+    }
+}
