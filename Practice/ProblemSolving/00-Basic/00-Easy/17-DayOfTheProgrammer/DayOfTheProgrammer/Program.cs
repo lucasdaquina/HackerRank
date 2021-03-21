4441f9458f35b640ecda3940891e1e1a101413ef
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-21 19:53:07 -0300

    Day of the Programmer test.

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/17-DayOfTheProgrammer/DayOfTheProgrammer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DayOfTheProgrammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string result = DayOfProgrammer(year);
+
+            Console.WriteLine(result);
+        }
+
+        static string DayOfProgrammer(int year)
+        {
+            var dateReturn = new DateTime();
+            if (year == 1918)
+            {
+                return $"26.09.1918";
+                //dateReturn = new DateTime(year, 1, 1).AddDays(223);
+            }
+            else if (year >= 1919)
+            {
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                {
+                    //dateReturn = new DateTime(year, 1, 1).AddDays(255);
+                    return $"12.09.{year}";
+                }
+                else
+                {
+                    //dateReturn = new DateTime(year, 1, 1).AddDays(255);
+                    return $"13.09.{year}";
+                }
+            }
+            else
+            {
+                if (year % 4 == 0)
+                {
+                    //dateReturn = new DateTime(year, 1, 1).AddDays(255);
+                    return $"12.09.{year}";
+                }
+                else
+                {
+                    //dateReturn = new DateTime(year, 1, 1).AddDays(255);
+                    return $"13.09.{year}";
+                }
+            }
+
+            //return $"{dateReturn.Day.ToString("dd")}.{dateReturn.Month.ToString().PadLeft(2, '0')}.{dateReturn.Year}";
+        }
+    }
+}
