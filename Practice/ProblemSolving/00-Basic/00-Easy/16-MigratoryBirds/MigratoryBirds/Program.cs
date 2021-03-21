b9ca306b7066f2bfdf2c7aa09dea52779440c9fd
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-21 14:58:06 -0300

    Migratory Birds Exercise

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/16-MigratoryBirds/MigratoryBirds/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MigratoryBirds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arrCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            int result = MigratoryBirds(arr);
+
+            Console.WriteLine(result);
+
+        }
+
+        static int MigratoryBirds(List<int> arr)
+        {
+            //1 2 3 4 5 4 3 2 1 3 4
+
+            //1 4 4 4 5 3
+            var aux = arr.OrderBy(_ => _).GroupBy(_ => _)
+                .Select(_ => new 
+                {
+                    index = _.Key,
+                    qtd = _.Count()
+                })
+                .ToList();
+
+            var maxCount = aux.Max(_ => _.qtd);
+
+            return aux.Where(_ => _.qtd == maxCount).Min(_ => _.index);
+        }
+    }
+}
