3eb2714ab62c6ae17b96314aa2390a0ec8ebedef
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-22 20:47:10 -0300

    Bill Division Test

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/18-BillDivision/BillDivision/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BillDivision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nk = Console.ReadLine().TrimEnd().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            List<int> bill = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(billTemp => Convert.ToInt32(billTemp)).ToList();
+
+            int b = Convert.ToInt32(Console.ReadLine().Trim());
+
+            BonAppetit(bill, k, b);
+        }
+
+        static void BonAppetit(List<int> bill, int k, int b)
+        {
+            var itemToRemove = bill[k];
+            bill.Remove(itemToRemove);
+
+            var sumBill = bill.Sum() / 2;
+
+            if (sumBill == b)
+            {
+                Console.WriteLine("Bon Appetit");
+            }
+            else
+            {
+                Console.WriteLine(b - sumBill);
+            }
+
+        }
+
+
+    }
+}
