d936fde5fca4ccfb68ef18cb482e24cecd9ced06
Lucas Dáquina <lucasdaquina@gmail.com>
2021-03-22 21:31:50 -0300

    Sales by Match Exercise

---
+++ Practice/ProblemSolving/00-Basic/00-Easy/19-SalesByMatch/SalesByMatch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace SalesByMatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+            int result = SockMerchant(n, ar);
+
+            Console.WriteLine(result);
+        }
+
+        static int SockMerchant(int n, int[] ar)
+        {
+            var result = 0;
+            var listAr = ar.ToList().GroupBy(_ => _).Select(_ => new { qtd = _.Count(), index = _.Key }).Where(_ => _.qtd > 1).ToList();
+
+            foreach(var a in listAr)
+            {
+                result = result + a.qtd / 2;
+            }
+            return result;
+        }
+
+    }
+}
